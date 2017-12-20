3ea84aebd8026841e480906079d9e00744c5502e
rayraib <binitarai11@gmail.com>
2017-12-20 02:32:02 +0000

    comment out free statements

--- check_no_arg_func.c
+++ check_no_arg_func.c
@@ -4,9 +4,10 @@
 * @line_num: Number line of the arg in bytecode file
 * Return: O success, -1 if invalid command
 */
-int check_no_arg_func(unsigned int line_num, char *token_1)
+int check_no_arg_func(unsigned int line_num, char *token_1, char *buf, FILE *fp)
 {
 	int i;
+	
 	instruction_t no_arg_func[] = {
 			{"pall", pall},
 			{NULL, NULL}
@@ -16,6 +17,8 @@ int check_no_arg_func(unsigned int line_num, char *token_1)
 		if (strcmp(token_1, no_arg_func[i].opcode) == 0)
 		{
 			no_arg_func[i].f(&head, line_num);
+			free (buf);
+			fclose (fp);
 			return (0);
 		}
 	}
@@ -27,28 +30,36 @@ int check_no_arg_func(unsigned int line_num, char *token_1)
 * @token_1: first argument to check
 * @token_2: second argument to check
 */
-void check_arg_func(unsigned int line_num, char *token_1, char *token_2)
+void check_arg_func(unsigned int line_num, char *token_1, char *token_2, char *buf, FILE *fp)
 {
 	int i, n;
-	stack_t *stack = NULL;
+	stack_t *stack;
+	
 	instruction_t arg_func[] ={
 			{"push", push},
 			{NULL, NULL}
 	};
-
+	stack = NULL;
+	(void) buf;
+	(void) fp;
 	for (i = 0; arg_func[i].opcode != NULL; i++)
 	{
 		if (strcmp(token_1, arg_func[i].opcode) == 0)
 		{
 			if (token_2 != NULL && isdigit(*token_2) != 0)
 			{
+				
 				n = atoi(token_2);
-				stack = create_stack(n);
+				stack = create_stack(n, buf, fp);
 				arg_func[i].f(&stack, line_num);
+				/*free (buf);
+				fclose (fp);*/
 			}
 		}
 		else
 		{
+			free(buf);
+			fclose(fp);
 			printf("L%d: unknown instruction %s\n", line_num, token_2);
 			exit(EXIT_FAILURE);
 		}
