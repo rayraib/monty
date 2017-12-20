67a4f620e5a1fb39b7a735414bf9f7c7dae39ff6
rayraib <binitarai11@gmail.com>
2017-12-20 20:48:38 +0000

    add prototype for sub function

--- monty.h
+++ monty.h
@@ -48,4 +48,6 @@ void pint(stack_t **head, unsigned int line_num);
 void pop(stack_t **head, unsigned int line_num);
 void swap(stack_t **head, unsigned int line_num);
 void add(stack_t **head, unsigned int line_num);
+void nop(stack_t **head, unsigned int line_num);
+void sub(stack_t **head, unsigned int line_num);
 #endif
