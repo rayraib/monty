718003f0fb6c250a553d627d95bdc6cff0248feb
rayraib <binitarai11@gmail.com>
2017-12-20 20:26:09 +0000

    add swap function

--- stack_ops.c
+++ stack_ops.c
@@ -12,7 +12,7 @@ void pint(stack_t **head, unsigned int line_num)
 		free_stack();
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n",(*head)->n);
+	printf("%d\n", (*head)->n);
 }
 /**
 * pop - Remove top element from stack
@@ -38,4 +38,25 @@ void pop(stack_t **head, unsigned int line_num)
 		free((*head)->prev);
 		(*head)->prev = NULL;
 	}
+}
+/**
+* swap - swaps the top two elements of the stack
+* @head: Pointer to the top of teh element of the stack
+* @line_num: Line number of the command in monty bytecode file
+*/
+void swap(stack_t **head, unsigned int line_num)
+{
+	if (head == NULL || (*head) == NULL || (*head)->next == NULL)
+	{
+		printf("L%d: can't swap, stack too short\n", line_num);
+		free_stack();
+		exit(EXIT_FAILURE);
+	}
+	(*head)->next = (*head)->next->next;	
+	(*head) = (*head)->next->prev;
+	(*head)->next = (*head)->prev;
+	(*head)->prev = NULL;
+	(*head)->next->prev = (*head);
+	if((*head)->next->next != NULL)
+		(*head)->next->next->prev = (*head)->next;
 }
