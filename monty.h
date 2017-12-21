3c609160f022562c595a40947bbe958f9ff01793
rayraib <binitarai11@gmail.com>
2017-12-21 23:20:49 +0000

    add prototype for pchar function

--- monty.h
+++ monty.h
@@ -56,4 +56,5 @@ void mul(stack_t **head, unsigned int line_num);
 void invalid_cmd_errmsg(char *cmd, unsigned int ln_num, char *buf, FILE *fp);
 stack_t *check_int_arg(char *arg, char *buf, FILE *fp);
 void div_stack(stack_t **head, unsigned int line_num);
+void pchar(stack_t **head, unsigned int line_num);
 #endif
