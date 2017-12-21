eb6dbe3f62f3d85e3224a59f6de8cdf78a1655e4
rayraib <binitarai11@gmail.com>
2017-12-21 23:59:23 +0000

    add mod function

--- stack_op_math.c
+++ stack_op_math.c
@@ -89,3 +89,30 @@ void div_stack(stack_t **head, unsigned int line_num)
 	(*head)->prev = NULL;
 	(*head)->n = n;
 }
+/**
+* mod - computes the rest of division of the second element by first element
+* @head: Pointer to the top of teh element of the stack
+* @line_num: Line number of the command in monty bytecode file
+*/
+void mod(stack_t **head, unsigned int line_num)
+{
+	int n = 0;
+
+	if (head == NULL || (*head) == NULL || (*head)->next == NULL)
+	{
+		printf("L%d: can't mod, stack too short\n", line_num);
+		free_stack();
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->n == 0)
+	{
+		printf("L%d: division by zero\n", line_num);
+		free_stack();
+		exit(EXIT_FAILURE);
+	}
+	n = (*head)->next->n % (*head)->n;
+	(*head) = (*head)->next;
+	free((*head)->prev);
+	(*head)->prev = NULL;
+	(*head)->n = n;
+}
