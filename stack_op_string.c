dc4f1c097a825f9371b20821934e0155a6399463
rayraib <binitarai11@gmail.com>
2017-12-22 20:52:04 +0000

    add function rotr

--- stack_op_string.c
+++ stack_op_string.c
@@ -93,3 +93,27 @@ void rotl(stack_t **head, unsigned int line_num)
 		tmp->next->prev = tmp;
 	}
 }
+/**
+* rotr - rotates stack to the bottom
+* @head: Pointer to a pointer that points to the stack
+* @line_num: Number line of the command rotl in the monty file
+*/
+void rotr(stack_t **head, unsigned int line_num)
+{
+	stack_t *tmp;
+	(void) line_num;
+
+	if (head != NULL && (*head) != NULL && (*head)->next != NULL)
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		(*head)->prev = tmp;
+		tmp->next = (*head);
+		tmp->prev->next = NULL;
+		tmp->prev = NULL;
+		(*head) = tmp;
+	}
+}
