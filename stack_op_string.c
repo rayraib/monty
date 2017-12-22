c9d3e1b4627248acf13ebc8cb3b4ea7a5744b062
rayraib <binitarai11@gmail.com>
2017-12-22 19:10:04 +0000

    add new function rotl

--- stack_op_string.c
+++ stack_op_string.c
@@ -69,3 +69,27 @@ void pstr(stack_t **head, unsigned int line_num)
 		putchar('\n');
 	}
 }
+/**
+* rotl - rotates stack to the top
+* @head: Pointer to a pointer that points to the stack
+* @line_num: Number line of the command rotl in the monty file
+*/
+void rotl(stack_t **head, unsigned int line_num)
+{
+	stack_t *tmp;
+	(void) line_num;
+
+	if (head != NULL && (*head) != NULL && (*head)->next != NULL)
+	tmp = *head;
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		(*head) = (*head)->next;
+		tmp->next = (*head)->prev;
+		(*head)->prev = NULL;
+		tmp->next->next  = NULL;
+		tmp->next->prev = tmp;
+	}
+}
